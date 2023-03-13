05abbe5ae36ae3f97445d76ba39af08f48d1ba66
Tomas Padilla <padillatomasagustin@gmail.com>
2023-03-13 16:54:14 +0100

    eurekaserver - Accounts consuming Cards and Loans. Docker Compose

--- service-accounts/src/main/java/com/padillatomas/accounts/controller/AccountsController.java
+++ service-accounts/src/main/java/com/padillatomas/accounts/controller/AccountsController.java
@@ -7,10 +7,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.padillatomas.accounts.config.AccountsServiceConfig;
-import com.padillatomas.accounts.model.Accounts;
-import com.padillatomas.accounts.model.Customer;
-import com.padillatomas.accounts.model.Properties;
+import com.padillatomas.accounts.model.*;
 import com.padillatomas.accounts.repository.AccountsRepository;
+import com.padillatomas.accounts.service.client.CardsFeignClient;
+import com.padillatomas.accounts.service.client.LoansFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 /**
@@ -31,6 +32,8 @@ public class AccountsController {
 
 	private final AccountsRepository accountsRepository;
 	private final AccountsServiceConfig accountsConfig;
+	private final CardsFeignClient cardsFeignClient;
+	private final LoansFeignClient loansFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -52,6 +55,20 @@ public class AccountsController {
 				accountsConfig.getActiveBranches()
 				);
 		return ow.writeValueAsString(properties);
+	}
+
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setCards(cards);
+		customerDetails.setLoans(loans);
+
+		return customerDetails;
 	}
 
 
---
+++ service-accounts/src/main/java/com/padillatomas/accounts/model/CustomerDetails.java
@@ -0,0 +1,20 @@
+package com.padillatomas.accounts.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class CustomerDetails {
+
+    private Accounts accounts;
+
+    private List<Cards> cards;
+
+    private List<Loans> loans;
+
+}
